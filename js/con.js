537ba3c1c09610e255d5a4782cd916d88bafa50b
ahmed398 <59135690+ahmed398@users.noreply.github.com>
2022-06-04 02:53:38 +0200

    Update con.js

--- js/con.js
+++ js/con.js
@@ -1,1 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAqcUKCs2rQYK5p-khDEGbY81UNgi-Qp_I",
+  authDomain: "nshop-1b107.firebaseapp.com",
+  databaseURL: "https://nshop-1b107-default-rtdb.firebaseio.com",
+  projectId: "nshop-1b107",
+  storageBucket: "nshop-1b107.appspot.com",
+  messagingSenderId: "447603697705",
+  appId: "1:447603697705:web:e6df55ddd9cb86a3531438",
+  measurementId: "G-75E9HR4F09"
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
